9999ae2e04ce7a56fa748700a00d7f1be5fc54a5
rohithjayarajan <rohith.jayarajan@gmail.com>
2018-11-29 16:22:09 -0500

    [Cleanup]: fix issue of method comments
    fix issue: [method comments should be part of the declaration in the
    header, not the source implementation.]

--- app/main.cpp
+++ app/main.cpp
@@ -17,9 +17,8 @@
 #include <PIDController.hpp>
 
 int main() {
-  // Created an object of class PIDController.
   PIDController PID;
-  // Calling the setGains function to set the values of gains Kp, Ki, and Kd.
+
   PID.setGainKp(0.4);
   PID.setGainKi(0.2);
   PID.setGainKd(0.01);
@@ -28,11 +27,10 @@ int main() {
   double setVelocity = 5.0;
   std::cout << "Initial velocity = " << actualVelocity << std::endl;
   std::cout << "Reference velocity = " << setVelocity << std::endl;
-  // Calling the computeVelocity function by setting the targetSetpoint
-  // and actualVelocity.
+
   actualVelocity = PID.computeVelocity(setVelocity, actualVelocity);
   std::cout << "Current velocity = " << actualVelocity << std::endl;
-  // Calling the getGains function to display gains.
+
   std::cout << "Values of PID gains are as follows" << std::endl;
   std::cout << "Proportional gain: " << PID.getGainKp() << std::endl;
   std::cout << "Integral gain: " << PID.getGainKi() << std::endl;
